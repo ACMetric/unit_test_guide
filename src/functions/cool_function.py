ac4e795ccc72bb4fa3beeeb37bc0f1fb64d0a4c6
Jakob Damen <jakob.damen@outlook.com>
2023-08-14 15:29:04 +0200

    Added parametrisation test

--- src/functions/cool_function.py
+++ src/functions/cool_function.py
@@ -2,25 +2,42 @@ from custom_errors import NegativeNumberException
 from numpy import sin
 
 
-def check_if_fun_number(num: int)-> bool:
+def check_if_fun_number(number: int)-> bool:
     """Checks if the given number is fun, returns true if number is fun
 
     Args:
-        num (int): input number
+        number (int): input number
 
     Returns:
         (bool, str): Result if it is a fun number, with explanation
     """    
     #Show that exception can also be tested
-    if num< 0:
-        raise NegativeNumberException(num)
+    if number< 0:
+        raise NegativeNumberException(number)
     
-    if (num % 2) == 0:
+    if (number % 2) == 0:
         return False
     
     #Add case we will not test to show code coverage
-    if num == 17:
+    if number == 17:
         return False
     
     return True
 
+
+def check_multiple_numbers(numbers: list[int]) -> list[bool]:
+    """Loop over a selection of numbers to check if fun
+
+    Args:
+        numbers (list[int]): Numbers to check
+
+    Returns:
+        list[bool]: results of funness of numbers
+    """    
+    results = []
+    for index, number in enumerate(numbers):
+        result = check_if_fun_number(number)
+        results.append(result)
+        
+    return results
+    
--- src/functions/tests/test_cool_function.py
+++ src/functions/tests/test_cool_function.py
@@ -2,42 +2,60 @@ from functions.cool_function import check_if_fun_number
 import pytest
 from custom_errors import NegativeNumberException
 
-
-def test_number_equal_to_one()-> None:
-    """Checks if 1 is a fun number
-    """    
-    
-    #Arrange
-    input_value = 1
-    
-    #Act
-    result = check_if_fun_number(input_value)
-    
-    #Assert
-    expected_result = True
-    assert result == expected_result
-    
-def test_number_equal_to_two()-> None:
-    """Checks if 2 is not a fun number
-    """   
-     
-    #Arrange
-    input_value = 2
-    
-    #Act
-    result = check_if_fun_number(input_value)
-    
-    #Assert
-    expected_result = False
-    assert result == expected_result
-    
-def test_invalid_number()-> None:
-    """Test should trow error since invalid number
-    """    
-    
-    #Arrange
-    input_value = -2
-    
-    #Act and Assert
-    with pytest.raises(NegativeNumberException):
+class TestCheckIfFunNumber():
+    def test_number_equal_to_one(self)-> None:
+        """Checks if 1 is a fun number
+        """    
+        
+        #Arrange
+        input_value = 1
+        
+        #Act
         result = check_if_fun_number(input_value)
+        
+        #Assert
+        expected_result = True
+        assert result == expected_result
+        
+    def test_number_equal_to_two(self)-> None:
+        """Checks if 2 is not a fun number
+        """   
+        
+        #Arrange
+        input_value = 2
+        
+        #Act
+        result = check_if_fun_number(input_value)
+        
+        #Assert
+        expected_result = False
+        assert result == expected_result
+        
+    @pytest.mark.parametrize("input_value, expected_result", [(1, True), (2, False), (42, False)])
+    def test_parametrized_numbers(self, input_value, expected_result)-> None:
+        """Use parametrization to check multiple inputs and outputs in the same test
+        """   
+        
+        #Act
+        result = check_if_fun_number(input_value)
+        
+        #Assert
+        assert result == expected_result
+        
+    def test_invalid_number(self)-> None:
+        """Test should trow error since invalid number
+        """    
+        
+        #Arrange
+        input_value = -2
+        
+        #Act and Assert
+        with pytest.raises(NegativeNumberException):
+            result = check_if_fun_number(input_value)
+            
+class TestCheckMultipleNumbers():
+    def test_five_numbers(self) -> None:
+        print()
+        
+    def test_empty_input(self) -> None:
+        print()
